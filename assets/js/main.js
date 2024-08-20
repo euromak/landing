4c515ca4e3aca85a6fd200daf2a930e4809b9721
VIktor <39494152+euromak@users.noreply.github.com>
2024-08-20 03:27:13 +0300

    Done countdown js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-
+/* установка маски на инпуты формы */
 function setInputMask(el) {
     let inputPhone = document.querySelectorAll('input[type="tel"]');
     let inputMask = new Inputmask({"mask" : '+7 (999) 999-99-99'}, "9", { repeat: 11 });
@@ -24,7 +24,8 @@ function validateInput(field) {
 
     return true;
 }
-/**/
+
+/* проверка статуса ответа от сервера*/
 function checkedForm(form,status) {
     if(status != 1) {
         form.telephone.classList.add('is-invalid');
@@ -34,6 +35,45 @@ function checkedForm(form,status) {
     }
 }
 
+/* Таймер обратного счета */
+function timer($value){
+    let timeNow = new Date();
+    let countDownDate = (timeNow.getDate() >= 15) ?
+            new Date(timeNow.getFullYear(),timeNow.getMonth()+1,1,24,0).getTime():
+            new Date(timeNow.getFullYear(),timeNow.getMonth(),15,24,0).getTime();
+
+    let result,
+        elDate = document.querySelector('.date .timer__block-value'),
+        elDateName = document.querySelector('.date .timer__block-name'),
+        elHour = document.querySelector('.hour .timer__block-value'),
+        elHourName = document.querySelector('.hour .timer__block-name'),
+        elMin = document.querySelector('.minute .timer__block-value'),
+        elMinName = document.querySelector('.minute .timer__block-name'),
+        elSec = document.querySelector('.second .timer__block-value'),
+        elSecName = document.querySelector('.second .timer__block-name');
+
+    let period = setInterval(function(){
+        let dateNow = new Date().getTime(),
+            difference = countDownDate - dateNow,
+            days = Math.floor(difference / (1000 * 60 * 60 * 24)),
+            hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+            minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60)),
+            seconds = Math.floor((difference % (1000 * 60)) / 1000);
+
+        elDate.textContent = days;
+        elHour.textContent = hours;
+        elMin.textContent = minutes;
+        elSec.textContent = seconds;
+
+        if (difference < 0) {
+            clearInterval(period);
+        }
+
+    },1000)
+
+    return result;
+}
+timer();
 window.addEventListener('submit', function(e){
     e.preventDefault();
     let formValues = new FormData(e.target);
@@ -54,7 +94,7 @@ window.addEventListener('submit', function(e){
 
 window.addEventListener('load', function(e) {
 
-    let cartSwiper = new Carousel(document.getElementById("myCarousel"), {
+    const cartSwiper = new Carousel(document.getElementById("myCarousel"), {
         // Your custom options
         Dots: false,
         Thumbs: {
